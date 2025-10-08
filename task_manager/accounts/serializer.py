748b7ab3d7195b9044150d3c029f07ebc202b644
Vineet0105 <vineetpokharna7@gmail.com>
2025-10-08 17:24:27 +0530

    Added Task

--- task_manager/accounts/serializer.py
+++ task_manager/accounts/serializer.py
@@ -1,21 +1,23 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from rest_framework.serializers import ValidationError
+from rest_framework.validators import UniqueValidator
 
-class RegisterSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    email = serializers.CharField()
-    password = serializers.CharField()
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(write_only=True, min_length=6)
 
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        if User.objects.filter(username=validated_data['username']).exists():
-            return ValidationError("Username already Exists")
-    
-        user = User.objects.create(username=validated_data['username'],
-                            email = validated_data['email']
-                            )
-        user.set_password(validated_data['password'])
-        user.save()
-
-        return validated_data
+        # Create the user using create_user to ensure password hashing
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
