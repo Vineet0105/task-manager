c70cf9eb4d2382f4d696a16486814fd655ed3988
Vineet0105 <vineetpokharna7@gmail.com>
2025-10-08 15:57:32 +0530

    Added Authentication

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    due_date = models.DateField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
