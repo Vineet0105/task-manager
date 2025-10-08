f86300d6f6b4fefd11ec84924e654a84d248a4b8
Vineet0105 <vineetpokharna7@gmail.com>
2025-10-08 17:16:01 +0530

    Added Task

--- task_manager/accounts/views.py
+++ task_manager/accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from .serializer import *
 from rest_framework.response import Response
 from rest_framework import status
+
 class RegisterView(APIView):
     def post(self,request):
         data = request.data
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework  import viewsets
+from .models import *
+from .serializer import *
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class TaskViewset(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer()
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user = self.request.user).order_by('-created_at')
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
