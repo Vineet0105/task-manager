8715546da952697797e304af45e3703401797ee1
Vineet0105 <vineetpokharna7@gmail.com>
2025-10-08 17:20:56 +0530

    Added Task

--- task_manager/api/urls.py
+++ task_manager/api/urls.py
@@ -2,9 +2,10 @@ from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from django.urls import path, include
 from accounts.views import *
 from rest_framework.routers import DefaultRouter
+from tasks.views import *
 
 router = DefaultRouter()
-router.register()
+router.register('tasks/',TaskViewset,basename='task')
 
 
 url_patterns = [
